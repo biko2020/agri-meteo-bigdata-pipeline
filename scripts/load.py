cb7c507ac11489c4d13719a6f492ceff48b93f3c
ait oufkir <aitoufkirbrahimab@gmail.com>
2026-01-19 18:53:52 +0100

    updated spark and postgres image in docker-compose.yml and dependecies in dockerfile

--- scripts/load.py
+++ scripts/load.py
@@ -1,14 +1,16 @@
-import psycopg2
 import pandas as pd
+from sqlalchemy import create_engine
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="agri_db",
-    user="postgres",
-    password="postgres"
-)
+# Créer une connexion SQLAlchemy
+engine = create_engine('postgresql://postgres:postgres@postgres:5432/agri_db')
 
-df = pd.read_parquet("data/processed/weather.parquet")
+# Lire le fichier Parquet
+df = pd.read_parquet("/app/data/processed/weather.parquet")
 
-df.to_sql("weather_kpi", conn, if_exists="replace", index=False)
-conn.close()
+# Charger dans PostgreSQL
+df.to_sql("weather_kpi", engine, if_exists="replace", index=False)
+
+print(f"✓ {len(df)} lignes chargées dans PostgreSQL")
+
+# Fermer la connexion
+engine.dispose()
